649480366c308f0f115edb09787c109a7121c57e
Mike <9483554+mb243@users.noreply.github.com>
2020-02-09 19:33:57 -0500

    Update make-heic.sh

--- make-heic.sh
+++ make-heic.sh
@@ -23,8 +23,23 @@ done
 # - Merge in any metadata provided in xmp files that exiftool can handle
 # Preserve the data location at the same time. 
 # More info: https://exiftool.org/faq.html#Q9
-for x in *.heic; do
-    y="${x%heic}xmp"
-    echo "merging exifdata for $x ..."
-    exiftool -overwrite_original -tagsFromFile "$y" -all:all "$x"
+for hfile in *.heic; do
+    xfile="${hfile%heic}xmp"
+    echo "$xfile XMP data to $hfile ..."
+    exiftool -overwrite_original -tagsFromFile "$xfile" -all:all "$hfile"
+    # hacky workaround for GPS coordinates not being accurate
+    # requires ggrep or grep with -P for perl regex matching
+    echo "  Recalculating GPS coords for $hfile ..."
+    GPSLatitude=$(ggrep -P "(?<=<exif:GPSLatitude>)[0-9.]*" -o "$xfile")
+    GPSLatitudeRef=$(ggrep -P "(?<=<exif:GPSLatitudeRef>)[A-Z]" -o "$xfile")
+    GPSLongitude=$(ggrep -P "(?<=<exif:GPSLongitude>)[0-9.]*" -o "$xfile")
+    GPSLongitudeRef=$(ggrep -P "(?<=<exif:GPSLongitudeRef>)[A-Z]" -o "$xfile")
+    echo "  Found Lat = ${GPSLatitude}${GPSLatitudeRef} Long = ${GPSLongitude}${GPSLongitudeRef}"
+    exiftool \
+        -m -overwrite_original \
+        -GPS:GPSLatitude="${GPSLatitude}" \
+        -GPS:GPSLatitudeRef=${GPSLatitudeRef} \
+        -GPS:GPSLongitude="${GPSLongitude}" \
+        -GPS:GPSLongitudeRef=${GPSLongitudeRef} \
+        "$hfile"
 done
